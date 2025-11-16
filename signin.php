622d80c513d798514885fa5f5a5f20961d2bf8b1
mujohannes <61717026+mujohannes@users.noreply.github.com>
2025-11-16 23:13:02 +0000

    signin start

---
+++ signin.php
@@ -0,0 +1,54 @@
+<?php
+require('vendor/autoload.php');
+
+use Jm\Webproject\App;
+use Jm\Webproject\Account;
+
+// create an app object based on App class
+$app = new App();
+
+$title = "Sign up to UniLibrary";
+$message = "Join our website";
+$success = null;
+if( empty($_SESSION["username"]) ) {
+    $user = null;
+}
+else {
+    $user = $_SESSION["username"];
+}
+
+// handle POST request from the signup form
+if( $_SERVER["REQUEST_METHOD"] == "POST" ) {
+    //$password2 = $_POST["confirm-password"];
+    // initialise Account class
+    $account = new Account();
+    $signup = $account -> create($email,$password1,$username,"Test","User");
+    // check if signup is successful
+    if( $signup == true ) {
+        // success
+        $success = true;
+        $_SESSION["email"] = $email;
+        $_SESSION["username"] = $username;
+        // update the user variable
+        $user = $_SESSION["username"];
+    }
+    else {
+        // failed
+        $success = false;
+    }
+}
+
+
+// create a template loader
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment( $loader );
+// load the template into memory
+$template = $twig -> load('signup.html.twig');
+// add some variables for twig to render
+echo $template -> render([
+    'title' => $title,
+    'message' => $message,
+    'success' => $success,
+    'user' => $user
+]);
+?>
