b8a53b63961baf1153cb807ce6f5342c531df8cb
mujohannes <61717026+mujohannes@users.noreply.github.com>
2025-11-17 00:05:21 +0000

    adding signin flow

---
+++ signin.php
@@ -0,0 +1,55 @@
+<?php
+require('vendor/autoload.php');
+
+use Jm\Webproject\App;
+use Jm\Webproject\Account;
+
+// create an app object based on App class
+$app = new App();
+
+$title = "Sign in to UniLibrary";
+$message = "Sign in to your account";
+$success = null;
+if( empty($_SESSION["username"]) ) {
+    $user = null;
+}
+else {
+    $user = $_SESSION["username"];
+}
+
+// handle POST request from the signup form
+if( $_SERVER["REQUEST_METHOD"] == "POST" ) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+   
+    $account = new Account();
+    $signin = $account -> login($email,$password);
+    // check if signin is successful
+    if( $signin ) {
+        // success
+        $success = true;
+        // $_SESSION["email"] = $email;
+        // $_SESSION["username"] = $username;
+        // // update the user variable
+        // $user = $_SESSION["username"];
+    }
+    else {
+        // failed
+        $success = false;
+    }
+}
+
+
+// create a template loader
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment( $loader );
+// load the template into memory
+$template = $twig -> load('signin.html.twig');
+// add some variables for twig to render
+echo $template -> render([
+    'title' => $title,
+    'message' => $message,
+    'success' => $success,
+    'user' => $user
+]);
+?>
--- src/Account.php
+++ src/Account.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace Jm\Webproject;
 
 use Exception;
 use Jm\Webproject\Database;
 
-class Account extends Database {
+class Account extends Database
+{
     public function __construct()
     {
         // initialise the database connection
@@ -17,8 +19,7 @@ class Account extends Database {
         $username,
         $first,
         $last
-        ) 
-    {
+    ) {
         $query = "
         INSERT INTO Account
         (Email,Password,Username,First,Last,Type)
@@ -26,24 +27,76 @@ class Account extends Database {
         (?,?,?,?,?,1)
         ";
         $hash = password_hash($password, PASSWORD_DEFAULT);
-        $statement = $this -> connection -> prepare($query);
-        $statement -> bind_param("sssss",$email,$hash,$username,$first,$last);
+        $statement = $this->connection->prepare($query);
+        $statement->bind_param("sssss", $email, $hash, $username, $first, $last);
         try {
-            if( !$statement -> execute() ) {
+            if (!$statement->execute()) {
                 throw new Exception("Oops! Something is wrong");
-            }
-            else {
+            } else {
                 // account is created
                 return true;
             }
-        }
-        catch( Exception $e ) {
+        } catch (Exception $e) {
             // do something
-            echo $e -> getMessage();
+            echo $e->getMessage();
             // failure to create account
             return false;
+        }
+    }
+
+    public function login($email, $password)
+    {
+        // find the user in the database
+        $query = "
+            SELECT 
+            id,
+            Email,
+            Username,
+            Password,
+            First,
+            Last,
+            Type
+            FROM Account
+            WHERE 
+            Email = ?
+            AND
+            Active = 1
+        ";
+        $statement = $this->connection->prepare($query);
+        $statement -> bind_param("s", $email);
+        try {
+            if(!$statement -> execute()) {
+                throw new Exception("database error");
+            }
+            else {
+                $result = $statement -> get_result();
+                $account_data = array();
+                while( $row = $result->fetch_assoc() ) {
+                    array_push($account_data,$row);
+                }
+            }
+        }
+        catch(Exception $e) {
+            echo $e -> getMessage();
+            exit();
+        }
+
+        // check account data
+        if( count($account_data) == 0 ) {
+            // account does not exist if the array length is 0
+            return false;
+        }
+        else {
+            // check the password
+            $account = $account_data[0];
+            if( !password_verify($password, $account["Password"] )) {
+                // password does not match the hash in database
+                return false;
+            }
+            else {
+                return $account;
+            }
         }
 
     }
 }
-?>
