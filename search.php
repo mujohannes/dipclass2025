f0b0ffddfe388ee486561f33f87ed0ce42527912
mujohannes <61717026+mujohannes@users.noreply.github.com>
2025-12-01 12:06:34 +0000

    Fixes for search page

--- search.php
+++ search.php
@@ -7,26 +7,53 @@ use Jm\Webproject\Search;
 // create an app object based on App class
 $app = new App();
 
-// user type
-if( empty($_SESSION["type"] ) ) {
-    $type = null;
+// variables for the page
+$title = "Search Results";
+$message = "Search results";
+$type = null;
+$user = null;
+$account_id = null;
+$search_result = [];
+$keywords = null;
+
+// username
+if( !empty($_SESSION["username"]) ) {
+    $user = $_SESSION["username"];
 }
-else {
+// user type
+if( !empty($_SESSION["type"] ) ) {
     $type = $_SESSION["type"];
+}
+// account id
+if(!empty($_SESSION['account_id'])) {
+    $account_id = $_SESSION["account_id"];
 }
 
 if( isset($_GET["query"]) ) {
-   $keyword = $_GET["query"];
+   $keywords = $_GET["query"];
+   $title = "Search results for " . $keywords;
    // initialise search class
    $search = new Search();
    // call getResults method and pass user keyword
-   $results = $search -> getResults($keyword);
-   foreach( $results as $item ) {
-    print_r($item);
-    echo "<br>";
-   }
+   $search_result = $search -> getResults($keywords);
 }
 else {
     echo "You are not searching";
 }
+
+// create a template loader
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment( $loader );
+// load the template into memory
+$template = $twig -> load('search.html.twig');
+// add some variables for twig to render
+echo $template -> render([
+    'title' => $title,
+    'message' => $message,
+    'search' => $search_result,
+    'user' => $user,
+    'type' => $type,
+    'account_id' => $account_id,
+    'keywords' => $keywords 
+]);
 ?>
--- src/Search.php
+++ src/Search.php
@@ -3,30 +3,42 @@
 namespace Jm\Webproject;
 
 use Exception;
-use Jm\Webproject\App;
 use Jm\Webproject\Database;
 
-class Search extends Database {
-    public function __construct() {
+class Search extends Database
+{
+    public function __construct()
+    {
         parent::__construct();
     }
-    public function getResults( $keyword ) {
+    public function getResults($keyword)
+    {
         $query = "SELECT 
                 id,
                 Title,
                 Cover,
-                Description
+                Description,
+                Author
                 FROM `Book` WHERE title LIKE ? ";
         $search_keyword = "%$keyword%";
-        $statement = $this -> connection -> prepare($query);
-        $statement -> bind_param("s", $search_keyword );
-        $statement -> execute();
-        $result = $statement -> get_result();
-        $search_result = array();
-        while( $row = $result -> fetch_assoc() ) {
-            array_push($search_result, $row);
+        $statement = $this->connection->prepare($query);
+        $statement->bind_param("s", $search_keyword);
+        try {
+            if (!$statement->execute()) {
+                throw new Exception("Database error");
+            } 
+            else {
+                $result = $statement->get_result();
+                $search_result = array();
+                while ($row = $result->fetch_assoc()) {
+                    array_push($search_result, $row);
+                }
+                return $search_result;
+            }
+        } 
+        catch (Exception $e) {
+            // there's an error 
+            return false;
         }
-        return $search_result;
     }
 }
-?>
